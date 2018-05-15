3853872879df71faf1262d5ef17853f9c5825c70
harshahcr <39063531+harshahcr@users.noreply.github.com>
2018-05-15 17:41:12 +0530

    Add files via upload

---
+++ Session2_Assignment2.2_Pattern using loop.py
@@ -0,0 +1,13 @@
+#Draw pyramid displaying stars using nested loop
+
+n=int(input('Enter the number for building the pyramid :'))
+
+for i in range(n):
+    for j in range(i):
+        print ('* ', end="")
+    print('')
+
+for i in range(n,0,-1):
+    for j in range(i):
+        print('* ', end="")
+    print('')
